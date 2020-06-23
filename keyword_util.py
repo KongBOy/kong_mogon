8b64061064360ad1426b4c1a7bd709fd9f01da85
KongBoy <s89334roy@yahoo.com.tw>
2020-06-24 00:20:23 +0800

    加了太多東西忘記了，但目前可以做到的事情是 可以關鍵字搜尋、排序、shop搜尋囉！然後也有加了很多使用的例子拉！也能夠下載RIZING的所有Mallet囉！真的所有喔！一個個系列仔下來且合併這樣子！

--- keyword_util.py
+++ keyword_util.py
@@ -1,28 +1,15 @@
-from mogon_base_util import Products, Scraper_util, PLATFORM, RW_to_file
+from mogon_base_util import Products, Scraper_util, PLATFORM, RW_to_file, Search_obj
 
-class Keyword:
+class Keyword(Search_obj):
     def __init__(self):
         self.platform = None     ### 基本
         self.keyword = None      ### 基本
         self.keyword_url = None  ### 由上兩個基本來設定
 
         self.prods_dir = None    ### 設定 Keyword的prods_dir放哪裡
-        self.products = None     ### 建立 products物件 (放product的容器 和 記錄一些 來源url/目的地資料夾 的東西)
 
-    def __str__(self):
-        prod_string = ""
-        for go_prod, prod in enumerate(self.products.prods): prod_string += f"%04i {prod.prod_title}\n"%go_prod
-        return prod_string
-
-
-    ### 分成 get_all_page_prods 和 
-    ###      get_all_page_prods_imgs 兩階段是因為：如果再抓多頁面時，經常被擋，如果寫在一起，就要一起全部重新開始，所以才分成兩階段喔！
-    def get_all_page_prods(self):
-        Scraper_util.get_all_page_prods(self.products)
-
-    def get_all_page_prods_img(self, prod_num=True, prod_title=False, shop_id=False):
-        Scraper_util.get_all_page_prods_img(self.products, prod_num, prod_title, shop_id)
-
+    def _init_search_obj(self, base_url, prods_dir): ### 初始化 search_obj ( 建立products物件 )
+        super().__init__(base_url=base_url, prods_dir=prods_dir)
 
 
 class Keyword_builder:
@@ -36,10 +23,10 @@ class Keyword_builder:
         self.keyword_obj.keyword_url = url
 
         ### 設定目的地資料夾
-        self.keyword_obj.prods_dir = f"{self.keyword_obj.keyword}/{self.keyword_obj.platform.value}"
+        self.keyword_obj.prods_dir = f"keyword_search/{self.keyword_obj.keyword}/{self.keyword_obj.platform.value}"
 
         ### 建立products物件
-        self.keyword_obj.products = Products(base_url=self.keyword_obj.keyword_url, prods_dir=self.keyword_obj.prods_dir)
+        self.keyword_obj._init_search_obj(base_url=self.keyword_obj.keyword_url, prods_dir=self.keyword_obj.prods_dir)
 
     def build(self, platform, keyword):
         self.keyword_obj.platform = platform
@@ -47,23 +34,137 @@ class Keyword_builder:
         self._set_and_build_dir()
         return self.keyword_obj 
 
+### dummy的寫法還是留著好了，給以後的自己當例子參考要怎麼拆開簡單寫
+### example1：走訪3個平台，看看keyword能找到什麼，無腦用法 只能重新開始 爬url存成prods.txt 再 存圖，  
+###             如果想讀之前存的結果時， 爬url和存的兩行註解掉才可以， 要不然 總共會有 read的 和 爬下來的 prods 重複的兩份prods喔！
+def exp1_visit_3_platform_download_imgs_dummy(keyword):
+    key_at_jraku = Keyword_builder().build( PLATFORM.jraku, keyword) ### 建立 keyword物件
+    key_at_jraku.get_all_page_prods()                    ### 爬url存成prods.txt
+    RW_to_file.write_prods_from_search_obj(key_at_jraku) ### 把 prods 存入檔案
+    # RW_to_file.read_prods_to_search_obj(key_at_jraku)    ### 取得 之前存的 prods 檔案 ### 這行註解打開的話，要把前兩行註解掉喔！
+    key_at_jraku.get_all_page_prods_img(prod_num=False, prod_title=True, shop_name=True, price=True)
 
-key_playwood = Keyword_builder().build( PLATFORM.jraku, "playwood") ### 建立 shop_kurosa，但爬下來沒有且不像樂器店
-key_playwood.get_all_page_prods()
-RW_to_file.write_shop_prods(key_playwood) ### 把 shop.prods 存入檔案
-RW_to_file.read_shop_prods(key_playwood) ### 取得 之前存的 shop.prods 檔案
-key_playwood.get_all_page_prods_img(prod_num=False, prod_title=True, shop_id=True)
+    key_at_yahoobid = Keyword_builder().build( PLATFORM.yahoobid, keyword) ### 建立 keyword物件
+    key_at_yahoobid.get_all_page_prods()                    ### 爬url存成prods.txt
+    RW_to_file.write_prods_from_search_obj(key_at_yahoobid) ### 把 prods 存入檔案
+    # RW_to_file.read_prods_to_search_obj(key_at_yahoobid)    ### 取得 之前存的 prods 檔案 ### 這行註解打開的話，要把前兩行註解掉喔！
+    key_at_yahoobid.get_all_page_prods_img(prod_num=False, prod_title=True, shop_name=True, price=True)
 
-key_playwood = Keyword_builder().build( PLATFORM.yahoobid, "playwood") ### 建立 shop_kurosa，但爬下來沒有且不像樂器店
-key_playwood.get_all_page_prods()
-RW_to_file.write_shop_prods(key_playwood) ### 把 shop.prods 存入檔案
-RW_to_file.read_shop_prods(key_playwood) ### 取得 之前存的 shop.prods 檔案
-key_playwood.get_all_page_prods_img(prod_num=False, prod_title=True, shop_id=True)
+    key_at_yahooshop = Keyword_builder().build( PLATFORM.yahooshop, keyword) ### 建立 keyword物件
+    key_at_yahooshop.get_all_page_prods()                    ### 爬url存成prods.txt
+    RW_to_file.write_prods_from_search_obj(key_at_yahooshop) ### 把 prods 存入檔案
+    # RW_to_file.read_prods_to_search_obj(key_at_yahooshop)    ### 取得 之前存的 prods 檔案 ### 這行註解打開的話，要把前兩行註解掉喔！
+    key_at_yahooshop.get_all_page_prods_img(prod_num=False, prod_title=True, shop_name=True, price=True)
+### exp用法：給 keyword即可
+# exp1_visit_3_platform_download_imgs_dummy("playwood")
+# exp1_visit_3_platform_download_imgs_dummy("playwood m-")
+# exp1_visit_3_platform_download_imgs_dummy("playwood m- RIZING")
+# exp1_visit_3_platform_download_imgs_dummy("playwood m RIZING")
+# exp1_visit_3_platform_download_imgs_dummy("playwood RIZING")
+# exp1_visit_3_platform_download_imgs_dummy("playwood RIZING")
 
-key_playwood = Keyword_builder().build( PLATFORM.yahooshop, "playwood") ### 建立 shop_kurosa，但爬下來沒有且不像樂器店
-key_playwood.get_all_page_prods()
-RW_to_file.write_shop_prods(key_playwood) ### 把 shop.prods 存入檔案
-RW_to_file.read_shop_prods(key_playwood) ### 取得 之前存的 shop.prods 檔案
-key_playwood.get_all_page_prods_img(prod_num=False, prod_title=True, shop_id=True)
+############################################################################################################################################
+### example2：走訪3個平台，看看keyword能找到什麼，用一些包好的function，會先看之前有沒有存prods.txt，有就用之前的，沒有重爬
+def exp2_visit_3_platform_download_imgs_and_use_before_things(keyword, restart_url=False):
+    key_at_jraku = Keyword_builder().build( PLATFORM.jraku, keyword) ### 建立 keyword物件
+    key_at_jraku.use_b_download_prods_img(restart_url=restart_url, prod_num=False, prod_title=True, shop_name=True, price=True)
+
+    key_at_yahoobid = Keyword_builder().build( PLATFORM.yahoobid, keyword) ### 建立 keyword物件
+    key_at_yahoobid.use_b_download_prods_img(restart_url=restart_url, prod_num=False, prod_title=True, shop_name=True, price=True)
+
+    key_at_yahooshop = Keyword_builder().build( PLATFORM.yahooshop, keyword) ### 建立 keyword物件
+    key_at_yahooshop.use_b_download_prods_img(restart_url=restart_url, prod_num=False, prod_title=True, shop_name=True, price=True)
+### exp用法：給 keyword即可
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood")
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood m-")
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood m- RIZING")
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood m RIZING")
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood RIZING")
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood RIZING")
+
+### 想重新爬prods.txt，restart_url=True即可
+# exp2_visit_3_platform_download_imgs_and_use_before_things("playwood RIZING", restart_url=True)
+
+####################################################################################################################################
+### dummy的寫法還是留著好了，給以後的自己當例子參考要怎麼拆開簡單寫
+def exp3_write_to_docx_dummy(platform, keyword):
+    key_at_yahooshop = Keyword_builder().build( platform, keyword) ### 建立 keyword物件
+    key_at_yahooshop.get_all_page_prods()                    ### 爬url存成prods.txt
+    RW_to_file.write_prods_from_search_obj(key_at_yahooshop) ### 把 prods 存入檔案
+    # RW_to_file.read_prods_to_search_obj(key_at_yahooshop)    ### 取得 之前存的 prods 檔案 ### 這行註解打開的話，要把前兩行註解掉喔！
+    key_at_yahooshop.get_all_page_prods_img(prod_num=False, prod_title=True, shop_name=True, price=True)
+    RW_to_file.write_prods_from_search_obj_to_word(key_at_yahooshop)
+### exp用法： 給 platform 和 keyword即可
+# exp3_write_to_docx_dummy( PLATFORM.yahooshop, "playwood RIZING")
 
 
+####################################################################################################################################
+def exp4_write_to_docx_and_use_before_things(platform, keyword, restart_url=False, restart_img=False, sort_key=None):
+    key_at_yahooshop = Keyword_builder().build( platform, keyword) ### 建立 keyword物件
+    key_at_yahooshop.use_c_write_to_word(restart_url=restart_url, restart_img=restart_img, sort_key=sort_key)
+### exp用法： 給 platform 和 keyword即可
+#  (restart_url=False, restart_img=False) ### 一開始 prods.txt和prods_img 什麼都沒有的時候 會自動重爬  或者 如果 prods.txt 和 prods_img 都已經確定好對應了，直接寫docx
+#  (restart_url=True)                     ### prods.txt 和 prods_img 都重新下載，再寫 docx
+#  (restart_url=False, restart_img=True)  ### 用現存的prods.txt，重新下載 prods_img，再寫 docx
+### 2020/06/20 最後用這個囉！
+# exp4_write_to_docx_and_use_before_things( PLATFORM.yahooshop, "playwood RIZING", restart_url=True, sort_key="prod_title")
+
+def exp5_manually_search_RIZING_mallet():
+    m_1000 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-1001 RIZING")
+    m_3000 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-3000 RIZING")
+    m_6011 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-6011 RIZING")
+    m_6021 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-6021 RIZING")
+    m_7001 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-7001 RIZING")
+    m_611 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-611 RIZING")
+    m_2001 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-2001 RIZING")
+    m_4000 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-4000 RIZING")
+    m_5000 = Keyword_builder().build(PLATFORM.yahooshop, "playwood M-5000 RIZING")
+    xg_series = Keyword_builder().build(PLATFORM.yahooshop, "playwood xg RIZING")
+    b_series = Keyword_builder().build(PLATFORM.yahooshop, "playwood b-1 RIZING")
+    xb_series = Keyword_builder().build(PLATFORM.yahooshop, "playwood xb RIZING")
+    m_101 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-101 RIZING")
+    m_201 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-201 RIZING")
+    m_301 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-301 RIZING")
+    m_401 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-401 RIZING")
+    m_501 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-501 RIZING")
+    m_801 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-801 RIZING")
+    m_901 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-901 RIZING")
+    two_tone = Keyword_builder().build(PLATFORM.yahooshop, "playwood two-tone RIZING")
+    m_01 = Keyword_builder().build(PLATFORM.yahooshop, "playwood m-01 RIZING")
+    sck = Keyword_builder().build(PLATFORM.yahooshop, "playwood sck RIZING")
+    sc = Keyword_builder().build(PLATFORM.yahooshop, "playwood sc RIZING")
+    
+    searchs = [m_1000, m_3000, m_6011, m_6021, m_7001, m_611, m_2001, m_4000, m_5000, xg_series, 
+               b_series, m_101, m_201, m_301, m_401, m_501, m_801, m_901, two_tone, m_01, sck, sc]
+    
+    combine_prods = Keyword_builder().build(PLATFORM.yahooshop, "combine")
+    for search in searchs:
+        search.get_all_page_prods()
+        search.sort_by_key(sort_key="prod_title")
+        combine_prods.products.prods +=  search.products.prods
+    combine_prods.use_c_write_to_word(restart_img=True)
+    
+    
+exp5_manually_search_RIZING_mallet()
+####################################################################################################################################
+### 一些以前的例子，還是保留一下好了，以後忘記怎麼 簡單的寫可以參考看看～～
+### 從 step0 可以找到 適合的 keyword 和 平台，這裡就可專心 寫成word囉！
+# def step1a_download_prods_and_write_file(keyword_obj):
+#     keyword_obj.get_all_page_prods() 
+#     RW_to_file.write_prods_from_search_obj(keyword_obj) ### 把 shop.prods 存入檔案
+
+# def step1b_read_from_file_and_download_imgs(keyword_obj):
+#     RW_to_file.read_prods_to_search_obj(keyword_obj) ### 取得 之前存的 shop.prods 檔案
+#     keyword_obj.get_all_page_prods_img()
+
+# def step1c_read_from_file_and_write_to_word(keyword_obj):
+#     RW_to_file.read_prods_to_search_obj(keyword_obj) ### 取得 之前存的 shop.prods 檔案
+#     RW_to_file.write_prods_from_search_obj_to_word(keyword_obj)
+
+# def step1_combine(keyword_obj): ### 因為各階段容易出錯，所以分成abc三個function，不想做哪段註解掉即可！
+#     # step1a_download_prods_and_write_file(keyword_obj)
+#     # step1b_read_from_file_and_download_imgs(keyword_obj)
+#     step1c_read_from_file_and_write_to_word(keyword_obj)
+
+# playwood_rizing = Keyword_builder().build( PLATFORM.yahooshop, "playwood RIZING") ### 建立 key_at_yahooshop，但爬下來沒有且不像樂器店
+# step1_combine(playwood_rizing)
--- shop_util.py
+++ shop_util.py
@@ -3,30 +3,18 @@ import sys
 sys.path.append(r"C:\Users\TKU\Desktop\kong_model2\kong_util")
 from build_dataset_combine import Check_dir_exist_and_build, Check_dir_exist_and_build_new_dir
 
+from mogon_base_util import Products, Scraper_util, RW_to_file, PLATFORM, Search_obj
 
-from mogon_base_util import Products, Scraper_util, RW_to_file, PLATFORM
-
-
-class Shop:
+class Shop(Search_obj):
     def __init__(self):
         self.platform = None  ### 基本
-        self.shop_id = None   ### 基本
+        self.shop_name = None   ### 基本
         self.shop_url = None  ### 由上兩個基本來設定
 
         self.prods_dir = None ### 設定 shop的prods_dir放哪裡
-        self.products = None  ### 建立 products 物件 (放product的容器 和 記錄一些 來源url/目的地資料夾 的東西)
-    
-    def get_all_page_prods(self):
-        Scraper_util.get_all_page_prods(self.products)
 
-    def get_all_page_prods_img(self, prod_num=True, prod_title=False, shop_id=False):
-        Scraper_util.get_all_page_prods_img(self.products, prod_title=prod_title)
-
-    def __str__(self):
-        prod_string = ""
-        for go_prod, prod in enumerate(self.products.prods): prod_string += f"%04i {prod.prod_title}\n"%go_prod
-        return prod_string
-        
+    def _init_search_obj(self, base_url, prods_dir): ### 初始化 search_obj ( 建立products物件)
+        super().__init__(base_url=base_url, prods_dir=prods_dir)
 
 class Shop_builder:
     def __init__(self, shop=None):
@@ -36,28 +24,26 @@ class Shop_builder:
     def _set_and_build_shop_dir(self):
         ### 設定url
         url = f"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action={self.shop.platform.value}&"
-        if  (self.shop.platform == PLATFORM.jraku): url += f"shopCode={self.shop.shop_id}"
-        elif(self.shop.platform == PLATFORM.yahooshop):   url += f"store={self.shop.shop_id}"
-        elif(self.shop.platform == PLATFORM.yahoobid):        url += f"seller={self.shop.shop_id}"
+        if  (self.shop.platform == PLATFORM.jraku): url += f"shopCode={self.shop.shop_name}"
+        elif(self.shop.platform == PLATFORM.yahooshop):   url += f"store={self.shop.shop_name}"
+        elif(self.shop.platform == PLATFORM.yahoobid):        url += f"seller={self.shop.shop_name}"
         self.shop.shop_url = url
 
         ### 設定目的地資料夾
-        self.shop.prods_dir = f"{self.shop.shop_id}/{self.shop.platform.value}"
+        self.shop.prods_dir = f"shop_search/{self.shop.shop_name}/{self.shop.platform.value}"
 
         ### 建立products物件
-        self.shop.products = Products(base_url=self.shop.shop_url, prods_dir=self.shop.prods_dir)
+        self.shop._init_search_obj(base_url=self.shop.shop_url, prods_dir=self.shop.prods_dir)
 
-    def build(self, platform, shop_id):
+    def build(self, platform, shop_name):
         self.shop.platform = platform
-        self.shop.shop_id = shop_id
+        self.shop.shop_name = shop_name
         self._set_and_build_shop_dir()
         return self.shop 
 
-
-
 class Shop_product_filter:
     @staticmethod
-    def filter_prods(shop, wants=[]):
+    def filter_title(shop, wants=[]):
         new_prods = []
         for prod in shop.products.prods:
             for want in wants:
@@ -65,29 +51,6 @@ class Shop_product_filter:
                     new_prods.append(prod)
         shop.products.prods = new_prods
 
-
-
-
-
-
-def step0_see_wants_title(shop):
-    shop.get_all_page_prods() 
-    RW_to_file.write_shop_prods(shop) ### 把 shop.prods 存入檔案
-    RW_to_file.read_shop_prods(shop) ### 取得 之前存的 shop.prods 檔案
-    shop.get_all_page_prods_img(prod_title=True)
-
-def step_write_to_word(shop):
-    shop.get_all_page_prods_img(prod_title=False)
-    RW_to_file.write_shop_prods(shop) ### 把 shop.prods 存入檔案
-    RW_to_file.write_shop_prods_to_word(shop)
-
-def step1_grab_prods_from_web(shop):
-    shop.get_all_page_prods() 
-    RW_to_file.write_shop_prods(shop) ### 把 shop.prods 存入檔案
-
-def step2_grab_prods_img_from_web_see_title(shop):
-    shop.prods = RW_to_file.read_shop_prods(shop) ### 取得 之前存的 shop.prods 檔案
-    shop.get_all_page_prods_img(prod_title=True)
 ###############################################################################
 ### 建立shop物件
 shop_kurosa       = Shop_builder().build( PLATFORM.jraku, "kurosawahonten") ### 建立 shop_kurosa，但爬下來沒有且不像樂器店
@@ -98,47 +61,58 @@ shop_kurosa       = Shop_builder().build( PLATFORM.jraku, "kurosawahonten") ### 
 # shop_mikidj       = Shop_builder().build( PLATFORM.jraku, "mikidj") ### 建立 mikidj ### 有m3001
 # shop_deeplearning = Shop_builder().build( PLATFORM.jraku, "deeplearning") ### 建立 deeplearning ### 有m3001，但爬下來沒有且不像樂器店
 
-step0_see_wants_title(shop_kurosa)
-# step0_see_wants_title(shop_kurosa)
-# step0_see_wants_title(shop_rizing)
-# step0_see_wants_title(shop_deeplearning)
-# step0_see_wants_title(shop_mikidj)
-# step0_see_wants_title(shop_soarsound)
-# step0_see_wants_title(shop_merry_net)
-# step0_see_wants_title(shop_auc_rizing)
-###############################################################################
-### 取得 shop內所有頁面的products
-# shop_kurosa.get_all_page_prods() 
+if(__name__=="__main__"):
+    ### 看看各個 shop的 產品價錢 和 產品名稱，可以從這裡看 wants使用的title
+    # shop_kurosa      .use_b_download_prods_img(price=True, prod_title=True )
+    # shop_rizing      .use_b_download_prods_img(price=True, prod_title=True )
+    # shop_deeplearning.use_b_download_prods_img(price=True, prod_title=True )
+    # shop_mikidj      .use_b_download_prods_img(price=True, prod_title=True )
+    # shop_soarsound   .use_b_download_prods_img(price=True, prod_title=True )
+    # shop_merry_net   .use_b_download_prods_img(price=True, prod_title=True )
+    # shop_auc_rizing  .use_b_download_prods_img(price=True, prod_title=True )
 
-########################################################################################################################################################
-### 用 prods 來 抓圖片
-# shop_kurosa.get_all_page_prods_img()
+    ### 寫入docx
+    # shop_kurosa.use_c_write_to_word(restart_url=True)                     ### prods.txt 和 prods_img 都重新下載，再寫 docx
+    # shop_kurosa.use_c_write_to_word(restart_url=False, restart_img=True)  ### 用現存的prods.txt，重新下載 prods_img，再寫 docx
+    # shop_kurosa.use_c_write_to_word(restart_url=False, restart_img=False) ### 如果 prods.txt 和 prods_img 都已經確定好對應了，直接寫docx
 
-########################################################################################################################################################
-### 要有 文字 和 圖片 才能寫進word裡
-### 這部分的參數要用 絕對位置喔！
-# RW_to_file.write_shop_prods_to_word(shop_kurosa)
+    ########################################################################################################################################################
+    ########################################################################################################################################################
+    ### 一些拆開寫的例子
 
-###############################################################################
-# RW_to_file.write_shop_prods(shop_kurosa) ### 把 shop.prods 存入檔案
-# print(shop_kurosa)  ### 看一下取得如何
+    ### 取得 shop內所有頁面的products
+    # shop_kurosa.get_all_page_prods() 
 
-###############################################################################
-### 如果已經抓過且有存起來不想花時間重新抓，用這裡的讀取上次存的結果
-# shop_kurosa.prods = RW_to_file.read_shop_prods(shop_kurosa) ### 取得 之前存的 shop.prods 檔案
-# print(shop_kurosa)  ### 看一下取得如何
+    ##################################################################################
+    ### 用 prods 來 抓圖片
+    # shop_kurosa.get_all_page_prods_img()
 
-###############################################################################
-# print(shop_kurosa)
-# Shop_product_filter.filter_prods(shop_kurosa, wants=["新品", "NEWS", "輸入盤", "【送料無料】"])
-# print(shop_kurosa)
+    ##################################################################################
+    ### 要有 文字 和 圖片 才能寫進word裡
+    ### 這部分的參數要用 絕對位置喔！
+    # RW_to_file.write_prods_from_search_obj_to_word(shop_kurosa)
+
+    ##################################################################################
+    # RW_to_file.write_prods_from_search_obj(shop_kurosa) ### 把 shop.prods 存入檔案
+    # print(shop_kurosa)  ### 看一下取得如何
+
+    ##################################################################################
+    ### 如果已經抓過且有存起來不想花時間重新抓，用這裡的讀取上次存的結果
+    # RW_to_file.read_prods_to_search_obj(shop_kurosa) ### 取得 之前存的 shop.prods 檔案
+    # print(shop_kurosa)  ### 看一下取得如何
+
+    ##################################################################################
+    ### filter 產品名稱
+    # Shop_product_filter.filter_title(shop_kurosa, wants=["新品", "NEWS", "輸入盤", "【送料無料】"])
+    # print(shop_kurosa)
 
 
-########################################################################################################################################################
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=rizing&page=" #40
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=liberty-it&page=" #100
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=merry-net&page=" #40
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=e-gakkinet&&page=" #100
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=gakki-de-genki&page="
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=kurosawahonten&page="
-###"http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=gakki-de-genki&page=40"
+    ########################################################################################################################################################
+    ### 以shop搜尋 的時候 網址大概的長相
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=rizing&page=" #40
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=liberty-it&page=" #100
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=merry-net&page=" #40
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=e-gakkinet&&page=" #100
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=gakki-de-genki&page="
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jrakutenshopping&shopCode=kurosawahonten&page="
+    ### "http://www.moganshopping.com/zh_tw/public/search/searchitem.php?action=jyahooshopping&store=gakki-de-genki&page=40"
